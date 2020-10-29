6355f47c985396332763e24572a810092c4628d3
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-29 12:19:07 +0530

    ADDED:Features to Find the Maximum of Three Integers

--- src/main/java/com/maximumofthreevariables/MaximumOfThreeVariables.java
+++ src/main/java/com/maximumofthreevariables/MaximumOfThreeVariables.java
@@ -1,11 +1,13 @@
 package com.maximumofthreevariables;
 
 public class MaximumOfThreeVariables {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Find maximum of Three Variables Problem");
+
+    public Integer findMaximumValue(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if(b.compareTo(max)>0)
+            max = b;
+        if(c.compareTo(max)>0)
+            max = c;
+        return max;
     }
 }
-
-
-
-
--- src/test/java/com/maximumofthreevariables/MaximumOfThreeVariablesTest.java
+++ src/test/java/com/maximumofthreevariables/MaximumOfThreeVariablesTest.java
@@ -1,6 +1,31 @@
 package com.maximumofthreevariables;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MaximumOfThreeVariablesTest {
+    @Test
+    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnFirstPositionValue(){
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Integer maximumValue = maximumOfThreeVariables.findMaximumValue(15, 10, 8);
+        System.out.println(maximumValue);
+        Assert.assertEquals((Object) 15,maximumValue);
+    }
 
+    @Test
+    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnSecondPositionValue() {
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Integer maximumValue = maximumOfThreeVariables.findMaximumValue(123, 150, 11);
+        System.out.println(maximumValue);
+        Assert.assertEquals((Object) 150,maximumValue);
+    }
 
+    @Test
+    public void givenThreeIntegers_WhenMaximumFound_ShouldReturnThirdPositionValue() {
+        MaximumOfThreeVariables maximumOfThreeVariables = new MaximumOfThreeVariables();
+        Integer maximumValue = maximumOfThreeVariables.findMaximumValue(123, 150, 200);
+        System.out.println(maximumValue);
+        Assert.assertEquals((Object) 200,maximumValue);
+
+    }
 }
