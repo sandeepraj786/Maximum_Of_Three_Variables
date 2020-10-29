53118f4f8668ba052a19d10473457e7762b49437
sandeep786 <sadeeprajsingh05@gmail.com>
2020-10-29 12:13:35 +0530

    ADDED: Greeting Message

---
+++ src/main/java/com/maximumofthreevariables/MaximumOfThreeVariables.java
@@ -0,0 +1,11 @@
+package com.maximumofthreevariables;
+
+public class MaximumOfThreeVariables {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Find maximum of Three Variables Problem");
+    }
+}
+
+
+
+
---
+++ src/test/java/com/maximumofthreevariables/MaximumOfThreeVariablesTest.java
@@ -0,0 +1,6 @@
+package com.maximumofthreevariables;
+
+public class MaximumOfThreeVariablesTest {
+
+
+}
